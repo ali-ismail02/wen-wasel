d6618b654abea2ab025d5492ac3b52dfc22217da
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-25 21:01:10 +0300

    Backend: Added PresavedRoute model

---
+++ wen-wasel/app/Models/PresavedRoute.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PresavedRoute extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'driver_id',
+        'name',
+        'start_time',
+    ];
+
+    public function driver(){
+        return $this->belongsTo(Driver::class);
+    }
+
+    public function route(){
+        return $this->hasMany(Route::class);
+    }
+}

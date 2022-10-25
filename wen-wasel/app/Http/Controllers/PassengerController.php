775ab4f0e774c3194f48150eaa0f1ecba902b827
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-25 23:09:08 +0300

    Backend: PassengerController => Added passenger signup API

--- wen-wasel/app/Http/Controllers/PassengerController.php
+++ wen-wasel/app/Http/Controllers/PassengerController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class PassengerController extends Controller
 {
-    //
+    public function passengerSignUp(Request $request){
+        if(!$request->name || !$request->phone || !$request->email || !$request->password){
+            return response()->json([
+                "status" => "0",
+                "message" => "Missing Fields"
+            ]);
+        }
+        if($user->where('email',$request['email'])->first()){
+            return response()->json([
+                "status" => "0",
+                "message" => "Email taken"
+            ]);
+        }
+        $user = new User();
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->user_type = 2;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->image = null;
+        $user->save();
+        return response()->json([
+            "status" => "1",
+            "message" => "User created successfully"
+        ]);
+    }
 }
--- wen-wasel/routes/api.php
+++ wen-wasel/routes/api.php
@@ -3,12 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PassengerController;
 
-Route::group(['prefix' => 'auth:api'], function () {
+Route::group(['middleware' => 'auth:api'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout',[AuthController::class, 'logout']);
     Route::post('refresh',[AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
 });
-
-Route::post('signUp', [UserController::class, 'signUp']);
+Route::group(['prefix' => 'user'], function () {
+    Route::post('signUp', [PassengerController::class, 'passengerSignUp']);
+});

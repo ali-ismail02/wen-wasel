0d2410f17358338367fd77b82dbc54981c189396
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-25 20:55:18 +0300

    Backend: Added Driver model

---
+++ wen-wasel/app/Models/Driver.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Driver extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'user_id',
+        'make',
+        'model',
+        'year',
+        'license_plate',
+        'license',
+        'front_image',
+        'side_image',
+        'seats',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function presavedRoutes(){
+        return $this->hasMany(PresavedRoute::class);
+    }
+    
+    public function routes(){
+        return $this->hasMany(Route::class);
+    }
+
+    public function tripRecords(){
+        return $this->hasMany(TripRecord::class);
+    }
+}

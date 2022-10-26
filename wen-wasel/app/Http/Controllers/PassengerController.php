ec2473867f879e465e925e3ba8d79c2c1b9d5733
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-26 19:28:01 +0300

    Backend: Added add trip API

--- wen-wasel/app/Http/Controllers/PassengerController.php
+++ wen-wasel/app/Http/Controllers/PassengerController.php
@@ -53,4 +53,53 @@ class PassengerController extends Controller
             "message" => "User created successfully"
         ]);
     }
+
+    public function addTrip(Request $request){
+
+        if(!$request->trip_infos || !$request->directions || !$request->transport_types){
+            return response()->json([
+                "status" => "0",
+                "message" => "Missing Fields"
+            ]);
+        }
+        
+        $trip_infos = explode("@",$request->trip_infos);
+        $directions = explode("@",$request->directions);
+        $transport_types = explode("@",$request->transport_types);
+
+        if(count($trip_infos) != count ($directions) || count($trip_infos) != count ($transport_types)){
+            return response()->json([
+                "status" => "0",
+                "message" => "Unmatched trip info"
+            ]);
+        }
+
+        $trip = new Trip();
+        $trip->user_id = $request->user_data->id;
+        $trip->save();
+
+        for($i=0;$i<count($trip_infos);$i++){
+            $trip_info_location = explode(",",$trip_infos[$i]);
+
+            $trip_info = new TripInfo();
+            $trip_info->start_location = $trip_info_location[0] . "," . $trip_info_location[1];
+            $trip_info->end_location = $trip_info_location[2] . "," . $trip_info_location[3];
+            $trip_info->departure_time = null;
+            $trip_info->arrival_time = null;
+            $trip_info->save();
+
+            $sub_trip = new SubTrip();
+            $sub_trip->trip_info_id = $trip_info->id;
+            $sub_trip->directions = $directions[$i];
+            $sub_trip->trip_type = $transport_types[$i];
+            $sub_trip->trip_id = $trip->id;
+            $sub_trip->save();
+        }
+
+        return response()->json([
+            "status" => "1",
+            "message" => "Trip added successfully",
+            "trip_id" => $trip->id
+        ]);
+    }
 }

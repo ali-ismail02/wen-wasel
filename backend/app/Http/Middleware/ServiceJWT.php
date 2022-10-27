f990fd711ff0199585b73602bbd596ef631e6915
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-27 22:19:35 +0300

    Backend: ServiceJWT => Added authentication and authorization

--- backend/app/Http/Middleware/ServiceJWT.php
+++ backend/app/Http/Middleware/ServiceJWT.php
@@ -3,19 +3,19 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use JWTAuth;
+use JWTFactory;
 use Illuminate\Http\Request;
 
 class ServiceJWT
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
     public function handle(Request $request, Closure $next)
     {
+        JWTAuth::parseToken()->authenticate();
+        $request['user_data'] = auth()->user();
+        if($request['user_data']->user_type != 4){
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
         return $next($request);
     }
 }

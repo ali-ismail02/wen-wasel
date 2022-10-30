b1a8c3bec565ed0dfa64cef5f1c1db6e5f3c8981
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-30 22:01:18 +0200

    Frontend: Added dummy Map component

---
+++ frontend/components/Map.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Map = () => {
+    return (
+        <View style={{height:300, backgroundColor: '#a0abff'}}>
+            <Text>This is a map</Text>
+        </View>
+    );
+};
+
+export default Map;
--- frontend/screens/HomeScreen.jsx
+++ frontend/screens/HomeScreen.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Map from "../components/Map";
 
 const HomeScreen = () => {
     return (
         <View>
-            <Text>Home Screen</Text>
+            <Map />
         </View>
     );
 };

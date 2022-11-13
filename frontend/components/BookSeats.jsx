ca7d47d3045d29ebfee6735492e10eaec5e36295
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-13 18:34:13 +0200

    Frontend: BookSeats => Added functionality to the buttons

--- frontend/components/BookSeats.jsx
+++ frontend/components/BookSeats.jsx
@@ -3,8 +3,22 @@ import styles from '../styles/styles';
 import GetTripType from '../hooks/GetTripType';
 import UserRouteOption from './UserRouteOption';
 import Button from './Button';
+import BookSeatsHook from '../hooks/BookSeat';
+import Booked from './Booked';
 
 const BookSeats = ({ path, setState }) => {
+
+    const bookSeat = async (route) => {
+        const response = await BookSeatsHook(route);
+        console.log(response);
+        if (response) {
+            console.log("Booking Successful");
+            setState("booked");
+            return
+        }
+        setState("failedBooking");
+    }
+
     for (let i = 0; i < path.length; i++) {
         if (GetTripType(path[i]) == "van") {
             return (
@@ -15,8 +29,8 @@ const BookSeats = ({ path, setState }) => {
                         </View>
                         <Text style={styles.instructions}>A van is available at this route with empty seats, would you like to book a seat?</Text>
                         <View style={styles.flex}>
-                            <Button text="Yes" onPress={() => { setState("booked") }} width= {"48%"} color={"#FF9E0D"}/>
-                            <Button text="No" onPress={() => { setState("pathConfirmed") }} width= {"48%"} color={"black"}/>
+                            <Button text="Yes" onPress={() => { bookSeat(path[i].element.trip) }} width={"48%"} color={"#FF9E0D"} />
+                            <Button text="No" onPress={() => { setState("noBooking") }} width={"48%"} color={"black"} />
                         </View>
                     </View>
                 </>

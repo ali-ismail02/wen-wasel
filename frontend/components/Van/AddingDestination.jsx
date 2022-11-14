368a2236f318694af7d9e449dc0eca5cbea27c35
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-14 20:14:00 +0200

    Frontend: AddingDestination => Linked to backend

--- frontend/components/Van/AddingDestination.jsx
+++ frontend/components/Van/AddingDestination.jsx
@@ -2,8 +2,30 @@ import { View, Text, Image } from 'react-native';
 import React from 'react';
 import styles from '../../styles/styles';
 import SelectDropdown from 'react-native-select-dropdown'
+import AddOneTimeRoute from '../../hooks/van/AddOneTimeRoute';
+import Button from '../Button';
 
 const AddingDestination = ({ setDestinations, setState, destination }) => {
+    const [minutes, setMinutes] = React.useState(null);
+    const [disabled, setDisabled] = React.useState(true);
+
+    const addDestination = async () => {
+        // get time and date after adding mins
+        let date = new Date();
+        date.setMinutes(date.getMinutes() + minutes);
+        let time = date.toLocaleTimeString();
+        let dateStr = date.toLocaleDateString();
+        const response = await AddOneTimeRoute(destination, dateStr + " " + time);
+        if (response) {
+            destination.id = response;
+            const dest = { latitude: destination.latitude, longitude:destination.longitude, id: response };
+            setDestinations(dest, dateStr + " " + time);
+            setState("destinationsSet");
+            return;
+        }
+    }
+
+
     const mins = new Array(60).fill(1).map((_, i) => i);
     return (
         <View style={styles.bottomPopupContainer}>
@@ -12,15 +34,10 @@ const AddingDestination = ({ setDestinations, setState, destination }) => {
                 <Image style={{ width: 50, height: 30 }} source={require('../../assets/images/van.png')} />
                 <Image style={{ width: 50, height: 20 }} source={require('../../assets/images/3dots.png')} />
                 <Image style={{ width: 30, height: 35 }} source={require('../../assets/images/waypoint.png')} />
-                <SelectDropdown data = {mins} 
-                    onSelect={(selectedItem, index) => { 
-                        // get time and date after adding mins
-                        let date = new Date();
-                        date.setMinutes(date.getMinutes() + selectedItem);
-                        let time = date.toLocaleTimeString();
-                        let dateStr = date.toLocaleDateString();
-                        setDestinations(destination,dateStr + " " + time );
-                        setState("destinationsSet");
+                <SelectDropdown data={mins}
+                    onSelect={(selectedItem, index) => {
+                        setMinutes(selectedItem);
+                        setDisabled(false);
                     }}
                     buttonStyle={styles.selectButton}
                     dropdownIconPosition={"right"}
@@ -34,6 +51,10 @@ const AddingDestination = ({ setDestinations, setState, destination }) => {
                     }}
 
                 />
+            </View>
+            <View style={[styles.flex, {paddingTop:10}]}>
+                <Button text="Cancel" onPress={() => setState("destinationsSet")} color={"black"} width={"47%"} />
+                <Button text="Add" onPress={addDestination} color={"#FF9E0D"} width={"47%"} disabled={disabled}/>
             </View>
         </View>
     );

1f87d41de617594e37f4b50595a6d72039a76dd1
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-14 15:48:07 +0200

    Frontend: Van Main => Added base states

--- frontend/components/Van/Main.jsx
+++ frontend/components/Van/Main.jsx
@@ -3,10 +3,21 @@ import React, { useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
 
 const Main = () => {
+    const [userState, setUserState] = useState("none");
+    const [allUserStates, setAllUserStates] = useState(["none"]);
+
+    const components = {
+        none: <></>,
+    }
+
+    const setState = (state) => {
+        setUserState(state);
+        setAllUserStates([...allUserStates, state]);
+    }
 
     return (
         <View>
-            <Text>Van Screen</Text>
+            {components[userState]}
         </View>
     );
 };

7100ce602b9683d82b448a2c7219a36b80845acf
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-14 15:35:56 +0200

    Frontend: UserScreen => Fixed import

--- frontend/screens/UserScreen.jsx
+++ frontend/screens/UserScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View } from "react-native";
-import Main from "../components/passenger";
+import Main from "../components/passenger/Main";
 
 const HomeScreen = () => {
     return (

c961e06d79f66b003f66e40b51d36b227cc9c367
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-14 20:13:10 +0200

    Frontend: AddOneTimeRoute => Fixed errors

--- frontend/hooks/van/AddOneTimeRoute.jsx
+++ frontend/hooks/van/AddOneTimeRoute.jsx
@@ -1,9 +1,11 @@
 import Post from "../Post";
-import {vanJWT} from "../vanJWT";
+import { JWT } from "../../constants/vanJWT";
 
 const AddOneTimeRoute = async (location,date_time) => {
-    const response = await Post('/api/van/addOneTimeRoute', {location,date_time}, vanJWT);
-    if(response.data.status === "success") {
+    date_time = date_time.replace(/\//g, "-");
+    let loc = location.latitude + "," + location.longitude;
+    const response = await Post('van/add-one-route', {location:loc,date_time}, JWT);
+    if(response.data.status === "Success") {
         return response.data.route.id;
     }
     return false;

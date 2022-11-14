4faa807a7d5f87d5626c8c5b9e1a8fcf8a35125c
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-14 17:30:51 +0200

    Frontend: van Route => Tweaked

--- frontend/components/Van/Main.tsx
+++ frontend/components/Van/Main.tsx
@@ -2,24 +2,17 @@ import React, { useState } from "react";
 import { View } from "react-native";
 import MapView, { LatLng } from 'react-native-maps';
 import CustomMap from "./CustomMap";
-import Route from "./Routes";
+import Routes from "./Routes";
 import { centerScreen } from "../../hooks/CameraChange";
 
 const Main = () => {
     const [userState, setUserState] = useState("none");
     const [allUserStates, setAllUserStates] = useState(["none"]);
     const [destination, setDestination] = useState<LatLng | null>(null);
-    const [allDestinations, setAllDestinations] = useState<LatLng[]>([]);
-    const [path, setPath] = useState<LatLng[] | null>([]);
+    const [allDestinations, setAllDestinations] = useState(null);
     const [location, setLocation] = useState(undefined);
     const [centerMap, setCenterMap] = useState(true);
-    const [paths, setPaths] = useState(undefined);
     const mapRef = React.useRef<MapView>(null);
-
-    const components = {
-        none: <Route destination={destination} userState={userState} path={path} />,
-
-    }
 
     const onPlaceSelect = (details) => {
         const position = {
@@ -29,7 +22,7 @@ const Main = () => {
         setDestination(position);
         centerScreen(location, position, mapRef, 150);
         setCenterMap(false);
-        setState("none");
+        setState("destinationsSet");
     }
 
     const setState = (state) => {
@@ -37,19 +30,19 @@ const Main = () => {
         setAllUserStates([...allUserStates, state]);
     }
 
-    const setDestinations = (destination) => {
-        setDestination(destination);
-        setAllDestinations([...allDestinations, destination]);
+    const setDestinations = (destination: LatLng, time: string) => {
+        setDestination(null);
+        setAllDestinations([...allDestinations, [destination, time]]);
         setState("destinationsSet");
-        if(path != null && path.length > 0) {
-            setPath([...path, destination]);
-            setState("pathSet");
-        }
+    }
+
+    const components = {
+        destinationsSet: <Routes destination={destination} setState={setState} destinations= {allDestinations}/>,
     }
 
     return (
         <View>
-            <CustomMap setState={setState} setLocation={setLocation} setCenterMap={setCenterMap} centerMap={centerMap} mapRef={mapRef} destination={destination} path={path} setDestination={setDestination} userState={userState} onPlaceSelect={onPlaceSelect}/>
+            <CustomMap setState={setState} setLocation={setLocation} setCenterMap={setCenterMap} centerMap={centerMap} mapRef={mapRef} destination={destination} setDestination={setDestination} userState={userState} onPlaceSelect={onPlaceSelect}/>
             {components[userState]}
         </View>
     );

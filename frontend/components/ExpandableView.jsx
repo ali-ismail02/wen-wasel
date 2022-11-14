b3f36fa68cd6ce58c94a6c2f95543a8944db0f0c
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-14 20:46:25 +0200

    Frontend: Added ExpandableView

---
+++ frontend/components/ExpandableView.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { Animated } from "react-native";
+
+const ExpandableView = ({ expanded = false }) => {
+    const [height] = useState(new Animated.Value(0));
+
+    useEffect(() => {
+        Animated.timing(height, {
+            toValue: !expanded ? 200 : 0,
+            duration: 150,
+            useNativeDriver: false
+        }).start();
+    }, [expanded, height]);
+
+    // console.log('rerendered');
+
+    return (
+        <Animated.View
+            style={{ height, backgroundColor: "orange" }}
+        ></Animated.View>
+    );
+};
+
+export default ExpandableView;

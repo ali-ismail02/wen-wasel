48233656b15713bb126647c2c15390877dc54073
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-15 19:41:56 +0200

    Frontend: van PathDraw => Added if arrived

--- frontend/components/Van/PathDraw.jsx
+++ frontend/components/Van/PathDraw.jsx
@@ -5,36 +5,38 @@ import MapViewDirections from "react-native-maps-directions";
 const PathDraw = (path, location) => {
     return (
         path.map((trip, index) => {
-            const dest = {latitude: parseFloat(trip[0].latitude), longitude: parseFloat(trip[0].longitude)}
-            if(index == 0) {
+            const dest = { latitude: parseFloat(trip[0].latitude), longitude: parseFloat(trip[0].longitude) }
+            if (trip[0].arrived === false) {
+                if (index == 0) {
+                    return (<>
+                        <MapViewDirections
+                            key={index}
+                            origin={location}
+                            destination={dest}
+                            apikey={Google_API_Key}
+                            strokeWidth={6}
+                            strokeColor="#FF9E0D"
+                            mode='DRIVING'
+                        />
+                        <Marker coordinate={dest} pinColor={"#FF9E0D"} />
+                    </>
+                    );
+                }
+                const origin = { latitude: path[index - 1][0].latitude, longitude: path[index - 1][0].longitude }
                 return (<>
                     <MapViewDirections
                         key={index}
-                        origin={location}
+                        origin={origin}
                         destination={dest}
                         apikey={Google_API_Key}
                         strokeWidth={6}
                         strokeColor="#FF9E0D"
                         mode='DRIVING'
                     />
-                    <Marker coordinate={dest} pinColor={"#FF9E0D"}/>
-                    </>
+                    <Marker coordinate={dest} pinColor={"#FF9E0D"} />
+                </>
                 );
             }
-            const origin = {latitude: path[index-1][0].latitude, longitude: path[index-1][0].longitude}
-            return (<>
-                <MapViewDirections
-                    key={index}
-                    origin={origin}
-                    destination={dest}
-                    apikey={Google_API_Key}
-                    strokeWidth={6}
-                    strokeColor="#FF9E0D"
-                    mode='DRIVING'
-                />
-                <Marker coordinate={dest} pinColor={"#FF9E0D"}/>
-                </>
-            );
         }
         )
     )

e8dfee8b7a44f3c2eee7fae0d62f76e47b04a798
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-15 19:26:55 +0200

    Frontend: van CustomMap => Called PathDraw

--- frontend/components/Van/CustomMap.jsx
+++ frontend/components/Van/CustomMap.jsx
@@ -1,12 +1,14 @@
-import INITIAL_POSITION from '../../constants/InitialPosition';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import styles from '../../styles/styles';
-import { moveTo } from '../../hooks/CameraChange';
-import { Google_API_Key } from '../../constants/GoogleAPIKey';
-import Search from '../Search';
 import { View } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { Google_API_Key } from '../../constants/GoogleAPIKey';
+import INITIAL_POSITION from '../../constants/InitialPosition';
+import { moveTo } from '../../hooks/CameraChange';
+import styles from '../../styles/styles';
+import Search from '../Search';
+import PathDraw from './PathDraw';
 
-const CustomMap = ({ setCenterMap, centerMap, mapRef, destination, setLocation, setState, setDestination, userState, onPlaceSelect }) => {
+const CustomMap = ({ setCenterMap, centerMap, mapRef, destination, setLocation, setState, setDestination, onPlaceSelect, location, allDestinations }) => {
 
     return (<>
         <MapView
@@ -29,6 +31,7 @@ const CustomMap = ({ setCenterMap, centerMap, mapRef, destination, setLocation, 
             style={styles.map}
             initialRegion={INITIAL_POSITION}>
             {destination != null && <Marker coordinate={destination} />}
+            {allDestinations != undefined && PathDraw(allDestinations, location) }
         </MapView>
         <View style={styles.searchContainer}>
             <Search onPlaceSelect={(details) => onPlaceSelect(details)} />

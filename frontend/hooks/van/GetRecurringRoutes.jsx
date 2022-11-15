b4f48bc97f017616326f670e301c0ad64e4cea06
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-15 14:38:33 +0200

    Frontend: van GetRecurringRoute => Fixed url

--- frontend/hooks/van/GetRecurringRoutes.jsx
+++ frontend/hooks/van/GetRecurringRoutes.jsx
@@ -2,7 +2,7 @@ import Get from "../Get";
 import { JWT } from "../../constants/vanJWT";
 
 const GetRecurringRoute = async (id) => {
-    const respone = await Get("van/get-recurring-route", JWT);
+    const respone = await Get("van/get-recurring-routes", JWT);
     if (respone.status === 200) {
         if(respone.data.presaved_routes.length > 0) {
             return respone.data.presaved_routes;

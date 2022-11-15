18fb5266583d2304923c7b8cf4aa78267d6dba1e
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-15 22:14:16 +0200

    Frontend: Added base files for redux

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,12 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import UserScreen from './screens/UserScreen';
-import VanScreen from './screens/VanScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { Entypo } from '@expo/vector-icons';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-        name="Home" 
-        component={VanScreen}
-        options={{
-          title: 'Wen Wasel',
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 32,
-          },
-          headerTitleAlign: 'center',
-          headerLeft: () => (<Entypo onPress={() => (alert("xx"))} name="menu" size={40} color="white" />)
-        }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+
+    </Provider>
   );
 }
 
---
+++ frontend/redux/slices/userSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface User {
+    userProfile?: {
+        type?: number,
+        id?: number;
+        image?: string;
+        name?: string;
+        email?: string;
+        phone?: string;
+    }
+}
+
+const initialState: User = {
+  userProfile: null
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    addUser(state, action) {
+        return action.payload;
+    },
+    deleteUser() {
+        return initialState;
+    },
+    updateUserProfile(state, action){
+        state.userProfile = action.payload.userProfile
+    },
+  },
+});
+
+export const { addUser, deleteUser, updateUserProfile } = userSlice.actions
+export default userSlice.reducer

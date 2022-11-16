2756b15cb1b3e9c6717654e242a33d5648dd345c
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-16 13:33:38 +0200

    Frontend: Added StackSwitcher

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,11 @@
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-
+import { StackSwitcher } from './navigation/StackSwitcher';
 
 export default function App() {
   return (
     <Provider store={store}>
-
+      <StackSwitcher />
     </Provider>
   );
 }

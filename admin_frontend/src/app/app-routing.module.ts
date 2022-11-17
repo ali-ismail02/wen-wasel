0a8b55e94605445766c04dc3e040872699e5df9a
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-17 17:45:27 +0200

    admin-frontend => Added base routing

--- admin_frontend/src/app/app-routing.module.ts
+++ admin_frontend/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PassengersComponent } from './pages/passengers/passengers.component';
+import { ServiceDriversComponent } from './pages/service-drivers/service-drivers.component';
+import { VanDriversComponent } from './pages/van-drivers/van-drivers.component';
 
 const routes: Routes = [
-  
+  {
+    path: 'passengers',
+    component: PassengersComponent,
+  },
+  {
+    path: 'van-drivers',
+    component: VanDriversComponent,
+  },
+  {
+    path: 'service-drivers',
+    component: ServiceDriversComponent,
+  },
+
 ];
 
 @NgModule({

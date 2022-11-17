7054d6dd406bbabeb1a9a7bde7e402edaeb15d1f
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-17 17:13:18 +0200

    Frontend: Passenger CustomMap => Previwed live location on map

--- frontend/components/passenger/CustomMap.jsx
+++ frontend/components/passenger/CustomMap.jsx
@@ -1,15 +1,16 @@
 import INITIAL_POSITION from '../../constants/InitialPosition';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import styles from '../../styles/styles';
-import {moveTo} from '../../hooks/CameraChange';
+import { moveTo } from '../../hooks/CameraChange';
 import PathDraw from './PathDraw';
 import { Google_API_Key } from '../../constants/GoogleAPIKey';
+import { Image } from 'react-native';
 
-const CustomMap = ({setCenterMap, centerMap, mapRef, destination, path, setLocation, setState, setDestination, userState}) => {
+const CustomMap = ({ setCenterMap, centerMap, mapRef, destination, path, setLocation, setState, setDestination, userState, liveLocations }) => {
     return (
         <MapView
             apikey={Google_API_Key}
-            provider={PROVIDER_GOOGLE} 
+            provider={PROVIDER_GOOGLE}
             showsUserLocation={true}
             onPanDrag={() => setCenterMap(false)}
             followsUserLocation={centerMap}
@@ -20,13 +21,22 @@ const CustomMap = ({setCenterMap, centerMap, mapRef, destination, path, setLocat
             }}
             onPress={(e) => {
                 setCenterMap(false);
-                if(userState != "searched") setState("searched")
+                if (userState != "searched") setState("searched")
                 setDestination(e.nativeEvent.coordinate)
             }}
-            ref={mapRef} 
-            style={styles.map} 
+            ref={mapRef}
+            style={styles.map}
             initialRegion={INITIAL_POSITION}>
             {destination != null && <Marker coordinate={destination} />}
+            {liveLocations.length > 0 && liveLocations.map((location, index) => {
+                console.log(location)
+                return <Marker coordinate={location.location}
+                    key={index}
+                    image={require('../../assets/images/van-icon.png')}
+                    
+                />
+            }
+            )}
             {path != undefined && PathDraw(path)}
         </MapView>
     );

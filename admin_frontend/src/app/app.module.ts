6aa3a99981e4cd3e4d9bf833e4dae8828cc8a73c
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-17 17:39:55 +0200

    admin-frontend => Added base service-drivers component

--- admin_frontend/src/app/app.module.ts
+++ admin_frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengersComponent } from './pages/passengers/passengers.component';
 import { VanDriversComponent } from './pages/van-drivers/van-drivers.component';
+import { ServiceDriversComponent } from './pages/service-drivers/service-drivers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PassengersComponent,
-    VanDriversComponent
+    VanDriversComponent,
+    ServiceDriversComponent
   ],
   imports: [
     BrowserModule,
---
+++ admin_frontend/src/app/pages/service-drivers/service-drivers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-drivers',
+  templateUrl: './service-drivers.component.html',
+  styleUrls: ['./service-drivers.component.css']
+})
+export class ServiceDriversComponent {
+
+}

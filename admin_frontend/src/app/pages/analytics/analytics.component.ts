1362daf3794a7011a3b8b08995bdc0489a416a01
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-18 18:46:17 +0200

    admin-frontend : analytics => Added base linking

--- admin_frontend/src/app/pages/analytics/analytics.component.ts
+++ admin_frontend/src/app/pages/analytics/analytics.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { environment } from 'src/environments/environment';
+import { ChartType } from 'angular-google-charts';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.css']
+  styleUrls: ['./analytics.component.css'],
 })
-export class AnalyticsComponent {
+export class AnalyticsComponent implements OnInit {
+  constructor() {}
 
+  most_popular_routes: any = {};
+  least_popular_routes: any = [];
+  most_active_van_drivers: any = [];
+  most_active_service_drivers: any = [];
+
+  most_array = [];
+  least_array = [];
+
+  LineChart: ChartType = ChartType.LineChart;
+
+  ngOnInit(): void {
+    try {
+      let token = localStorage.getItem('token');
+      axios
+        .get(environment.api + '/api/admin/get-analytics', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(({ data }) => {
+          this.most_popular_routes = data.most_popular_routes;
+          this.least_popular_routes = data.least_popular_routes;
+          this.most_active_van_drivers = data.most_active_van_drivers;
+          this.most_active_service_drivers = data.most_active_service_drivers;
+
+          let res: any = [];
+          Object.entries(this.most_popular_routes).forEach(([key, value]) =>
+            console.log(res.push([key, value]))
+          );
+          this.most_array = res;
+          res = [];
+          Object.entries(this.least_popular_routes).forEach(([key, value]) =>
+            console.log(res.push([key, value]))
+          );
+          this.least_array = res;
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }

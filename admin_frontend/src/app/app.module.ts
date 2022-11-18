afd1b84c66bcf11f8ada59ecbebea98f0769f85e
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-18 18:08:01 +0200

    admin-frontend : Added base Accept-admin components

--- admin_frontend/src/app/app.module.ts
+++ admin_frontend/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { PassengersComponent } from './pages/passengers/passengers.component';
 import { VanDriversComponent } from './pages/van-drivers/van-drivers.component';
 import { ServiceDriversComponent } from './pages/service-drivers/service-drivers.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AcceptAdminComponent } from './pages/accept-admin/accept-admin.component'
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
     PassengersComponent,
     VanDriversComponent,
     ServiceDriversComponent,
+    AcceptAdminComponent,
   ],
   imports: [
     BrowserModule,
---
+++ admin_frontend/src/app/pages/accept-admin/accept-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accept-admin',
+  templateUrl: './accept-admin.component.html',
+  styleUrls: ['./accept-admin.component.css']
+})
+export class AcceptAdminComponent {
+
+}

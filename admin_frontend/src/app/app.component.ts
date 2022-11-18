d259e0c12283e70773980f5a1027cdb8bce2e7c9
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-18 19:19:43 +0200

    admin-frontend : app => Fixed routing bug

--- admin_frontend/src/app/app.component.ts
+++ admin_frontend/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faSignOut, faTaxi } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'admin_frontend';
+export class AppComponent implements OnInit {
+  title = 'AdminPanel';
   logoutIcon = faSignOut;
   faTaxi = faTaxi;
+  _router: Router;
+
+  constructor(private router: Router) {
+    this._router = router;
+  }
+
+  ngOnInit(): void {
+    if (!localStorage.getItem('token')) this._router.navigateByUrl('/login');
+  }
+
+  handleSubmit(): void {
+    localStorage.clear();
+    this._router.navigateByUrl('/login');
+  }
 }

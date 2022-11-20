1b1353e27998b612b4c1f30e2b7b9eb57cc62288
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 22:49:01 +0200

    Frontend: UserRouteOption => Replaced  images with icons

--- frontend/components/passenger/UserRouteOption.jsx
+++ frontend/components/passenger/UserRouteOption.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { Image, Text, TouchableNativeFeedback, View } from "react-native";
 import GetFares from "../../hooks/passenger/GetFares";
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Feather from 'react-native-vector-icons/Feather';
 
 const UserRouteOption = ({ route, onPress, style, colorScheme }) => {
     const [time, setTime] = useState(undefined);
     const [price, setPrice] = useState(0);
+    const [color, setColor] = useState("black");
 
     // calculating time for route
     useEffect(() => {
+        {colorScheme == "dark" ? setColor("white") : setColor("black")}
         const getPrice = async () => {
             const price = await GetFares(route);
             setPrice(price);
@@ -19,7 +23,7 @@ const UserRouteOption = ({ route, onPress, style, colorScheme }) => {
         // add time to current time and set time to arrival time only withouth date
         date.setMinutes(date.getMinutes() + time);
         setTime(date.toLocaleTimeString());
-    }, [])
+    }, [colorScheme])
     // format price with commas
     const formatPrice = (price) => {
         return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -35,24 +39,21 @@ const UserRouteOption = ({ route, onPress, style, colorScheme }) => {
                                 if (trip.name.includes("service") && trip.name.includes("start")) {
                                     return (
                                         <View key={index} style={style.flex}>
-                                            {colorScheme == 'dark' ? <Image source={require(`../../assets/images/car_dark.png`)} style={{ width: 20, height: 20 }} /> :
-                                                <Image source={require(`../../assets/images/car.png`)} style={{ width: 20, height: 20 }} />}
+                                            <Icon name="taxi" size={20} color={color} />
                                             <Text style={style.routeOptionTripText}>{trip.cost}</Text>
                                         </View>
                                     );
                                 } else if (trip.name.includes("van") && trip.name.includes("start")) {
                                     return (
                                         <View key={index} style={style.flex}>
-                                        {colorScheme == 'dark' ? <Image source={require(`../../assets/images/van_dark.png`)} style={{ width: 20, height: 20 }} /> :
-                                            <Image source={require(`../../assets/images/van.png`)} style={{ width: 20, height: 20 }} />}
+                                            <Icon name="shuttle-van" size={20} color={color} />
                                             <Text style={style.routeOptionTripText}>{trip.cost}</Text>
                                         </View>
                                     );
                                 } else {
                                     return (
                                         <View key={index} style={style.flex}>
-                                        {colorScheme == 'dark' ? <Image source={require(`../../assets/images/walking_dark.png`)} style={{ width: 10, height: 20 }} /> :
-                                            <Image source={require(`../../assets/images/walking.png`)} style={{ width: 20, height: 20 }} />}
+                                            <Icon name="walking" size={20} color={color} />
                                             <Text style={style.routeOptionTripText}>{trip.cost}</Text>
                                         </View>
                                     );
@@ -60,8 +61,7 @@ const UserRouteOption = ({ route, onPress, style, colorScheme }) => {
                             } else {
                                 return (
                                     <View key={index} style={style.flex}>
-                                    {colorScheme == 'dark' ? <Image source={require(`../../assets/images/waypoint_dark.png`)} style={{ width: 20, height: 20 }} /> :
-                                        <Image source={require(`../../assets/images/waypoint.png`)} style={{ width: 20, height: 20 }} />}
+                                        <Feather name="map-pin" size={20} color={color} />
                                     </View>
                                 )
                             }

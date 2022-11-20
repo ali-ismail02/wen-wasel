8dac5d82b646a85c3621fd3959a2a881589f63e8
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 21:27:35 +0200

    Frontend: BookState => Renamed to BookedOrNoBookingState

--- frontend/components/passenger/BookedOrNoBookingState.jsx
+++ frontend/components/passenger/BookedOrNoBookingState.jsx
@@ -1,14 +1,14 @@
 import SubRides from "./SubRides";
 import Booked from "./Booked";
 
-const BookedState = ({ path, setPath, setState, setCenterMap, style, colorScheme }) => {
+const BookedOrNoBookingState = ({ path, setPath, setState, setCenterMap, style, colorScheme, status }) => {
 
     return (
         <>
             <SubRides path={path} setPath={setPath} setState={setState} setCenter={setCenterMap} style={style} colorScheme={colorScheme} />
-            <Booked status={1} style={style} />
+            <Booked status={status} style={style} />
         </>
     )
 }
 
-export default BookedState;
+export default BookedOrNoBookingState;

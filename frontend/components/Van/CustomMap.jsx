7d243d369412b0efb94afe58f1e9a854fb547b2e
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 20:09:46 +0200

    Frontend: CustomMap => Removed styles import, Added style prop

--- frontend/components/Van/CustomMap.jsx
+++ frontend/components/Van/CustomMap.jsx
@@ -1,25 +1,12 @@
-import { View, Appearance } from 'react-native';
+import { View } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Google_API_Key } from '../../constants/GoogleAPIKey';
 import INITIAL_POSITION from '../../constants/InitialPosition';
 import { moveTo } from '../../hooks/CameraChange';
-import styles from '../../styles/styles';
 import Search from '../Search';
 import PathDraw from './PathDraw';
-import {useEffect, useState} from 'react';
 
-const CustomMap = ({ setCenterMap, centerMap, mapRef, destination, setLocation, setState, setDestination, onPlaceSelect, location, allDestinations }) => {
-    const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
-    const [style, setStyle] = useState(styles.light);
-    
-    Appearance.addChangeListener(({ colorScheme }) => {
-        setColorScheme(colorScheme);
-        {}
-    });
-
-    useEffect(() => {
-        {colorScheme == 'dark' ? setStyle(styles.dark) : setStyle(styles.light)}
-    }, [colorScheme]);
+const CustomMap = ({ setCenterMap, centerMap, mapRef, destination, setLocation, setState, setDestination, onPlaceSelect, location, allDestinations, style, colorScheme }) => {
 
     return (<>
         <MapView
@@ -45,7 +32,7 @@ const CustomMap = ({ setCenterMap, centerMap, mapRef, destination, setLocation, 
             {allDestinations != undefined && PathDraw(allDestinations, location) }
         </MapView>
         <View style={style.searchContainer}>
-            <Search onPlaceSelect={(details) => onPlaceSelect(details)} />
+            <Search onPlaceSelect={(details) => onPlaceSelect(details)}  style={style} colorScheme={colorScheme} />
         </View>
     </>
     );

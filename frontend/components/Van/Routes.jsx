16a1ac725a2f5312df7909e7b4c3c706f152d3a7
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 20:10:19 +0200

    Frontend: Routes => Removed styles import, Added style prop

--- frontend/components/Van/Routes.jsx
+++ frontend/components/Van/Routes.jsx
@@ -1,26 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, FlatList, Appearance } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import styles from '../../styles/styles';
 import Button from '../Button';
 import RouteDescription from './RouteDescription';
 
-const Routes = ({ destination, destinations, setState, setDestinations }) => {
+const Routes = ({ destination, destinations, setState, setDestinations, style, colorScheme }) => {
     const [expanded, setExpanded] = useState(false);
     // get first destination from destinations where arrived is false
     const [firstDestination, setFirstDestination] = useState(destinations.find(destination => destination[0].arrived === false));
     const [finished, setFinished] = useState(true);
     const [disabled, setDisabled] = useState(false);
-    const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
-    const [style, setStyle] = useState(styles.light);
-    
-    Appearance.addChangeListener(({ colorScheme }) => {
-        setColorScheme(colorScheme);
-    });
-
-    useEffect(() => {
-        {colorScheme == 'dark' ? setStyle(styles.dark) : setStyle(styles.light)}
-    }, [colorScheme]);
 
     const getFirstDestination = () => {
         const firstDestination = destinations.find(destination => destination[0].arrived === false);
@@ -51,12 +40,12 @@ const Routes = ({ destination, destinations, setState, setDestinations }) => {
                     <View style={[style.flex, { paddingHorizontal: 30, paddingBottom: 10 }]}>
                         <Text style={style.subTitle}>Routes:</Text>
                         {destination == null ?
-                            firstDestination && <Button text="Delay" onPress={() => setState("delaying")} color={"black"} width={"80%"} /> :
+                            firstDestination && <Button text="Delay" onPress={() => setState("delaying")} color={"black"} width={"80%"} style={style} /> :
                             firstDestination ? <>
-                                <Button text="Delay" onPress={() => setState("delaying")} color={"black"} width={"35%"} />
-                                <Button text="Add Route" onPress={() => setState("addingRoute")} color={"#FF9E0D"} width={"35%"} />
+                                <Button text="Delay" onPress={() => setState("delaying")} color={"black"} width={"35%"} style={style} />
+                                <Button text="Add Route" onPress={() => setState("addingRoute")} color={"#FF9E0D"} width={"35%"} style={style} />
                             </> :
-                                <Button text="Add Route" onPress={() => setState("addingRoute")} color={"#FF9E0D"} width={"80%"} />
+                                <Button text="Add Route" onPress={() => setState("addingRoute")} color={"#FF9E0D"} width={"80%"} style={style} />
                         }
                     </View>
                     {expanded == true ?
@@ -65,7 +54,7 @@ const Routes = ({ destination, destinations, setState, setDestinations }) => {
                             renderItem={({ item }) => <RouteDescription destination={item} allDestionations={destinations} setDestinations={setDestinations} update={getFirstDestination} />}
                         /> :
                         firstDestination != undefined ?
-                            <RouteDescription destination={firstDestination} allDestionations={destinations} setDestinations={setDestinations} update={getFirstDestination} /> :
+                            <RouteDescription destination={firstDestination} allDestionations={destinations} setDestinations={setDestinations} update={getFirstDestination} style={style} colorScheme={colorScheme}  /> :
                             <View style={style.van.routeDescription}>
                                 <Text style={[style.subTitle, { width: "100%", textAlign: "center" }]}>No more destinations</Text>
                             </View>
@@ -83,7 +72,7 @@ const Routes = ({ destination, destinations, setState, setDestinations }) => {
                 <View style={style.van.routes}>
                     <View style={[style.flex, { paddingHorizontal: 30 }]}>
                         <Text style={style.subTitle}>Routes:</Text>
-                        <Button text="Add Route" onPress={() => setState("addingRoute")} color={"#FF9E0D"} width={"80%"} />
+                        <Button text="Add Route" onPress={() => setState("addingRoute")} color={"#FF9E0D"} width={"80%"} style={style} />
                     </View>
                 </View>
             </View>

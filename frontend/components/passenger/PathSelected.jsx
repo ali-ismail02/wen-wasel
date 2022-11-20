61d37c3920680d8ae9fe1f81b79fbd4870cfc3ff
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 21:14:17 +0200

    Frontend: Added PathSelected

---
+++ frontend/components/passenger/PathSelected.jsx
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import Button from "../Button";
+import { moveTo } from "../../hooks/CameraChange";
+
+const PathSelected = ({ style, setState, setCenterMap, location, mapRef }) => {
+
+    // handling path confirmation
+    const onPathConfirm = () => {
+        setState("pathConfirmed");
+        setCenterMap(true);
+        moveTo(location.coords, mapRef, 18);
+    }
+
+    return (
+    <View style={style.bottomPopupContainer}>
+        <Text style={style.instructions}>Confirm your route?</Text>
+        <Button text="Confirm Route" onPress={onPathConfirm} width={"100%"} color={"#FF9E0D"} style={style} />
+    </View>)
+}
+
+export default PathSelected;

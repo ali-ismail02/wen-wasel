16eba947079a9d6067b33690e7b3ab96dede1b5a
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 20:09:31 +0200

    Frontend: UserRouteOptions => Removed styles import, Added style prop

--- frontend/components/passenger/UserRouteOptions.tsx
+++ frontend/components/passenger/UserRouteOptions.tsx
@@ -4,25 +4,14 @@ import UserRouteOption from "./UserRouteOption";
 import { FlatList } from "react-native-gesture-handler";
 import React, { useState, useEffect } from "react";
 
-const UserRouteOptions = ({ routes, onPress }) => {
-    const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
-    const [style, setStyle] = useState<any>({bottomPopupContainer: null});
-    
-    Appearance.addChangeListener(({ colorScheme }) => {
-        setColorScheme(colorScheme);
-        {}
-    });
-
-    useEffect(() => {
-        {colorScheme == 'dark' ? setStyle(styles.dark) : setStyle(styles.light)}
-    }, [colorScheme]);
+const UserRouteOptions = ({ routes, onPress, style, colorScheme }) => {
     return (
         <View style={style.bottomPopupContainer}>
             <Text style={style.subTitle}>Routes:</Text>
             <FlatList style={style.flatList}
                 data={routes}
                 // render each route with UserRouteOption with onPress
-                renderItem={({ item }) => <UserRouteOption route={item} onPress={() => { onPress(item) }} />}
+                renderItem={({ item }) => <UserRouteOption route={item} onPress={() => { onPress(item) }} style={style} colorScheme={colorScheme}/>}
             />
         </View>
     );

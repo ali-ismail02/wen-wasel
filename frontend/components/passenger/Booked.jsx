a04f00ad9d87b1079c7f44f263500c4b044f7f55
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 20:13:30 +0200

    Frontend: Booking => Cleaned code. Added comments

--- frontend/components/passenger/Booked.jsx
+++ frontend/components/passenger/Booked.jsx
@@ -4,24 +4,25 @@ import { Image, Text, View } from 'react-native';
 const Booked = ({ status, style }) => {
     const [display, setDisplay] = useState(true);
 
-    if (status == 1) {
+    if (status == 1) { // Booking was successful
         return (<>
             {display == true &&
                 <View style={style.BookingPopup} >
                     <Image source={require('../../assets/images/tick.png')} style={style.bookingImage} />
                     <Text style={style.bookingText}>Booking Successful</Text>
                 </View>}
-        </>
-        )
+            </>
+            );
     }
+    // Booking was unsuccessful
     return (<>
         {display == true &&
             <View style={style.BookingPopup} >
                 <Image source={require('../../assets/images/x.png')} style={style.bookingImage} />
                 <Text style={style.bookingText}>Booking Failed</Text>
             </View>}
-    </>
-    )
+            </>
+            )
 }
 
 export default Booked;

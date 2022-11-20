528a5a080d75e980917d33a230e16ad5cdd546ea
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-20 20:35:45 +0200

    Frontend: Added BaseState Component

---
+++ frontend/components/passenger/BaseState.jsx
@@ -0,0 +1,34 @@
+import { View } from "react-native";
+import CenterMapButton from "../CenterMapButton";
+import { centerScreen } from "../../hooks/CameraChange";
+import { moveTo } from "../../hooks/CameraChange";
+import Search from "../Search";
+
+const BaseState = ({ style, setState, setSearchResult, setDestination, mapRef, location, setCenterMap }) => {
+
+    // handling place selection from search or map
+    const onPlaceSelect = (details) => {
+        const position = {
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng,
+        }
+        setSearchResult(position)
+        setDestination(position);
+        centerScreen(location, position, mapRef);
+        setCenterMap(false);
+        setState("searched");
+    }
+
+    return (<>
+        <View style={style.searchContainer}>
+            <Search onPlaceSelect={(details) => onPlaceSelect(details)} style={style} />
+        </View>
+        <CenterMapButton setCenterMap={setCenterMap}
+            moveTo={moveTo}
+            mapRef={mapRef}
+            location={location}
+            style={style} />
+    </>)
+}
+
+export default BaseState;

fafc4530828ca665e42b112a79b3a0858317e528
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-21 15:17:13 +0200

    Frontend: Main => Cleaned code. Added comments

--- frontend/components/Van/Main.tsx
+++ frontend/components/Van/Main.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { View, BackHandler,Appearance } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Appearance, BackHandler, View } from "react-native";
 import MapView, { LatLng } from 'react-native-maps';
-import CustomMap from "./CustomMap";
-import Routes from "./Routes";
-import { centerScreen } from "../../hooks/CameraChange";
-import AddingDestination from "./AddingDestination";
-import DelayingDestinations from "./DelayingDestinations";
-import GetRecurringRoute from "../../hooks/van/GetRecurringRoutes";
-import PresavedRoutesDropdown from "./PresavedRoutesDropdown";
-import SortPath from "../../hooks/van/SortPath";
 import { useSelector } from 'react-redux';
+import { centerScreen } from "../../hooks/CameraChange";
+import GetRecurringRoute from "../../hooks/van/GetRecurringRoutes";
+import SortPath from "../../hooks/van/SortPath";
 import styles from "../../styles/styles";
+import AddingDestination from "./AddingDestination";
+import CustomMap from "./CustomMap";
+import DelayingDestinations from "./DelayingDestinations";
+import PresavedRoutesDropdown from "./PresavedRoutesDropdown";
+import Routes from "./Routes";
 
 const Main = () => {
     const [userState, setUserState] = useState("none");
@@ -26,25 +26,21 @@ const Main = () => {
     const io = require("socket.io-client");
     const socket = io('http://192.168.1.50:5000');
     const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
-    const [style, setStyle] = useState<any>({bottomPopupContainer: null});
-    
+    const [style, setStyle] = useState<any>({ bottomPopupContainer: null });
+
     Appearance.addChangeListener(({ colorScheme }) => {
         setColorScheme(colorScheme);
-        {}
+        { }
     });
-
-    useEffect(() => {
-        {colorScheme == 'dark' ? setStyle(styles.dark) : setStyle(styles.light)}
-    }, [colorScheme]);
 
     const broadcastLocation = () => {
         // repeat this every 2 seconds
         setTimeout(() => {
-            if(location) {
+            if (location) {
                 socket.emit("location", {
                     type: "van",
                     id: id,
-                    location: {latitude: location?.latitude, longitude: location?.longitude},
+                    location: { latitude: location?.latitude, longitude: location?.longitude },
                 });
             }
             broadcastLocation();
@@ -52,6 +48,8 @@ const Main = () => {
     }
 
     useEffect(() => {
+        colorScheme == 'dark' ? setStyle(styles.dark) : setStyle(styles.light)
+        // get recurring routes from database
         const getRecurringRoutes = async () => {
             const recurringRoutes = await GetRecurringRoute();
             if (recurringRoutes) {
@@ -59,9 +57,11 @@ const Main = () => {
             }
         }
         getRecurringRoutes();
+        // call repeating function to broadcast location
         broadcastLocation();
-    }, []);
+    }, [colorScheme]);
 
+    // handle back button
     const backPressed = () => {
         setDestination(null);
         if (userState == "none") {
@@ -94,11 +94,13 @@ const Main = () => {
             return true;
         }
     }
+
     const backHandler = BackHandler.addEventListener(
         "hardwareBackPress",
         backPressed
     );
 
+    // handle adding destination using search bar
     const onPlaceSelect = (details) => {
         const position = {
             latitude: details.geometry.location.lat,
@@ -110,9 +112,10 @@ const Main = () => {
         setState("destinationsSet");
     }
 
+    // update user state and all user states
     const setState = (state) => {
         broadcastLocation();
-        if(state == "destinationsSet") {
+        if (state == "destinationsSet") {
             setAllUserStates(["none", "destinationsSet"]);
             setUserState("destinationsSet");
             return
@@ -121,6 +124,7 @@ const Main = () => {
         setAllUserStates([...allUserStates, state]);
     }
 
+    // handle adding destination
     const setDestinations = (destination, time) => {
         setDestination(null);
         setAllDestinations([...allDestinations, [destination, time]]);
@@ -128,16 +132,22 @@ const Main = () => {
         setState("destinationsSet");
     }
 
+    // handle delaying destination
     const updateAllDestinations = (destinations) => {
         setAllDestinations(destinations);
     }
 
+    // case switch for user state to render different components
     const components = (state) => {
         switch (state) {
-            case "none": return <PresavedRoutesDropdown presaved_routes={recurringRoutes} setUserState={setUserState} setAllDestinations={updateAllDestinations} style={style} />
-            case "destinationsSet": return <Routes destination={destination} setState={setState} destinations={allDestinations} setDestinations={updateAllDestinations} style={style} colorScheme={colorScheme} />
-            case "addingRoute": return <AddingDestination setDestinations={setDestinations} setState={setState} destination={destination} style={style} colorScheme={colorScheme}  />
-            case "delaying": return <DelayingDestinations destinations={allDestinations} setDestinations={updateAllDestinations} setState={setState} style={style} colorScheme={colorScheme}  />
+            case "none":
+                return <PresavedRoutesDropdown presaved_routes={recurringRoutes} setUserState={setUserState} setAllDestinations={updateAllDestinations} style={style} />
+            case "destinationsSet":
+                return <Routes destination={destination} setState={setState} destinations={allDestinations} setDestinations={updateAllDestinations} style={style} colorScheme={colorScheme} />
+            case "addingRoute":
+                return <AddingDestination setDestinations={setDestinations} setState={setState} destination={destination} style={style} colorScheme={colorScheme} />
+            case "delaying":
+                return <DelayingDestinations destinations={allDestinations} setDestinations={updateAllDestinations} setState={setState} style={style} colorScheme={colorScheme} />
         }
     }
 

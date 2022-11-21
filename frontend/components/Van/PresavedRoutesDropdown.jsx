c4862fd9b8eee87ab4272f358f07109cc245a3ad
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-21 17:31:01 +0200

    Frontend: PresavedRoutesDropdown => Tweaked styling

--- frontend/components/Van/PresavedRoutesDropdown.jsx
+++ frontend/components/Van/PresavedRoutesDropdown.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Image, Text, View } from "react-native";
 import SelectDropdown from "react-native-select-dropdown";
 import BuildRecurringRoutePath from "../../hooks/van/BuildRecurringRoutePath";
 import Button from "../Button";
 
 const PresavedRoutesDropdown = ({ presaved_routes, setAllDestinations, setUserState, style }) => {
-    const [selected_route, setSelectedRoute] = React.useState(null);
-    const [disabled, setDisabled] = React.useState(true);
+    const [selected_route, setSelectedRoute] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+    const [dropdown_style, setDropdownStyle] = useState({});
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+        const namesTemp = [];
+        for (let i = 0; i < presaved_routes.length; i++) {
+            namesTemp.push(presaved_routes[i].presaved_route.name);
+        }
+        setNames(namesTemp);
+        const styleTemp = {
+            maxHeight: 200,
+            height: namesTemp.length * 40 + 50,
+            ...style.dropdownStyle,
+        };
+        setDropdownStyle(styleTemp);
+    }, [presaved_routes]);
 
     const startRoute = async () => {
         BuildRecurringRoutePath(selected_route, setAllDestinations);
         setUserState("destinationsSet");
     }
 
-    names = [];
-    for (let i = 0; i < presaved_routes.length; i++) {
-        names.push(presaved_routes[i].presaved_route.name);
-    }
+
+
     return (
         <View style={style.bottomPopupContainerNoPadding}>
             <View style={[style.flex, { paddingHorizontal: 30, paddingVertical: 10, borderBottomColor: "#DDD", borderBottomWidth: 1 }]}>
                 <Text style={style.subTitle}>Presaved Routes:</Text>
                 <SelectDropdown
-                    rowStyle={style.dropdownRow}
                     data={names}
                     onSelect={(selectedItem, index) => {
                         setSelectedRoute(presaved_routes[index]);
                         setDisabled(false);
                     }}
+                    dropdownStyle={dropdown_style}
+                    rowStyle={style.dropdownRow}
                     buttonStyle={style.selectButtonRoutes}
                     selectedRowTextStyle={style.dropdownRowText}
                     rowTextStyle={style.dropdownRowText}

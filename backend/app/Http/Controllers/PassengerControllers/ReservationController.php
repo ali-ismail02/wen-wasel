48c81311c4be848e4a160db915734ee80f8269ce
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-30 18:00:23 +0200

    Backend: TripsController => Added addReservations API

--- backend/app/Http/Controllers/PassengerControllers/ReservationController.php
+++ backend/app/Http/Controllers/PassengerControllers/ReservationController.php
@@ -8,4 +8,62 @@ use Illuminate\Http\Request;
 class ReservationController extends Controller
 {
     //
+
+    // Api to add reservation
+    public function addReservation(Request $request){
+        // Validate the request
+        if($error = validate($request->all(), ['user_data' => 'required'])){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "Validation Failed",
+                "errors" => $error
+            ]);
+        }
+
+        if(!$route = Route::where('id',$request->route_id)->first()){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "Route not found"
+            ]);
+        }
+
+        // Get the driver of the route
+        $driver = $route->driver()->first();
+        if(!$driver){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "Driver not found"
+            ]);
+        }
+        // Make sure there are seats available
+        if($driver->seats == 0){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "No seats available"
+            ]);
+        }
+
+        // Add the reservation
+        $reservation = Reservation::create([
+            "user_id" => $request->user_data->id,
+            "route_id" => $request->route_id,
+            "status" => 0
+        ]);
+        if(!$reservation){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "Failed to add reservation"
+            ]);
+        }
+
+        // Update the driver seats
+        $driver->seats = $driver->seats - 1;
+        $driver->save();
+
+        return response()->json([
+            "status" => "success",
+            "message" => "Reservation added successfully",
+            "reservation" => $reservation
+        ]);
+    }
 }

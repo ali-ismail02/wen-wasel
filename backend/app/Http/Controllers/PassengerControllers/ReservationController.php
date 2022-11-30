f4b2e12f4327fa772d544b00c82a098a179eaed1
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-30 18:01:15 +0200

    Backend: TripsController => Added updateReservations API

--- backend/app/Http/Controllers/PassengerControllers/ReservationController.php
+++ backend/app/Http/Controllers/PassengerControllers/ReservationController.php
@@ -66,4 +66,77 @@ class ReservationController extends Controller
             "reservation" => $reservation
         ]);
     }
+    
+    // Api to get the reservations
+    public function getReservations(Request $request){
+        // Validate the request
+        if($error = validate($request->all(), ['user_data' => 'required'])){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "Validation Failed",
+                "errors" => $error
+            ]);
+        }
+
+        $user = $request->user_data;
+        // Get the reservations
+        $reservations = $user->reservations()->get();
+
+        $reservations_data = [];
+        foreach($reservations as $reservation){
+            $route = $reservation->route()->first();
+            $driver = $route->driver()->first();
+            $reservations_data[] = [
+                "reservation_id" => $reservation->id,
+                "route" => $route,
+                "driver" => $driver,
+                "status" => $reservation->status
+            ];
+        }
+
+        return response()->json([
+            "status" => "success",
+            "message" => "Reservations found",
+            "reservations" => $reservations_data
+        ]);
+    }
+
+    // Api to update reservation status to 1 (arrived)
+    public function updateReservation(Request $request){
+        // Validate the request
+        $rules = [
+            'user_data'=>'required',
+            'reservation_id'=>'required'
+        ];
+        if($error = validate($request->all(), $rules)){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "Validation Failed",
+                "errors" => $error
+            ]);
+        }
+        
+        $user = $request->user_data;
+
+        if(!$reservation = $user->reservations()->where('id',$request->reservation_id)->first()){
+            return response()->json([
+                "status" => "Failed",
+                "message" => "Reservation not found"
+            ]);
+        }
+
+        // Update the reservation status
+        $reservation->status = 1;
+        $reservation->save();
+
+        // Add seat to the driver
+        $driver = $reservation->route()->first()->driver()->first();
+        $driver->seats = $driver->seats + 1;
+        $driver->save();
+
+        return response()->json([
+            "status" => "success",
+            "message" => "Reservation updated successfully"
+        ]);
+    }
 }

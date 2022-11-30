286e0cd9cd9c332d3f2e3996e61ddf3578489c3f
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-30 18:10:52 +0200

    Backend: TripsController => Added getCurrentTrip API

--- backend/app/Http/Controllers/PassengerController.php
+++ backend/app/Http/Controllers/PassengerController.php
@@ -46,68 +46,7 @@ class PassengerController extends Controller
     
     // Api to get the cuurent trip if any
     public function getCurrentTrip(Request $request){
-        // Validate the request
-        if($error = validate($request->all(), ['user_data' => 'required'])){
-            return response()->json([
-                "status" => "Failed",
-                "message" => "Validation Failed",
-                "errors" => $error
-            ]);
-        }
-
-        $user = $request->user_data;
-        // Get the latest trip
-        $trips = $user->trips()->orderBy('id',"DESC")->get();
-        
-        $trips_data = [];
-        // Save the last trip in the trips array
-        $trip = $trips[0];
-
-        // Get the sub trips
-        $sub_trips = $trip->subTrips()->get();
-        $trip_infos = [];
-        // Get the trip info for each sub trip
-        foreach($sub_trips as $sub_trip){
-            $trip_info = $sub_trip->tripInfo()->first();
-            $trip_infos[] = [
-                "trip_info_id" => $trip_info->id,
-                "start_location" => $trip_info->start_location,
-                "end_location" => $trip_info->end_location,
-                "departure_time" => $trip_info->departure_time,
-                "arrival_time" => $trip_info->arrival_time,
-                "directions" => $sub_trip->directions,
-                "trip_type" => $sub_trip->trip_type
-            ];
-        }
-        $trips_data[] = [
-            "trip_id" => $trip->id,
-            "trip" => $trip_infos
-        ];
-
-        // Check if any sub trip has no arrival time
-        $current_trip = null;
-        foreach($trips_data as $trip){
-            foreach($trip['trip'] as $trip_info){
-                if($trip_info['arrival_time'] == null){
-                    $current_trip = $trip;
-                    break;
-                }
-            }
-        }
-
-        // If no current trip found
-        if($current_trip == null){
-            return response()->json([
-                "status" => "Failed",
-                "message" => "No current trip"
-            ]);
-        }
-
-        return response()->json([
-            "status" => "success",
-            "message" => "Trips found",
-            "current_trip" => $current_trip
-        ]);
+        return app("App\Http\Controllers\PassengerControllers\TripsController")->getCurrentTrip($request);
     }
 
     // Api to add reservation

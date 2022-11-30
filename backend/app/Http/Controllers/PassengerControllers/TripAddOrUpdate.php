bff844ce6aaefc6fdabd5e373b7f0a9fa3a7c35d
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-30 17:45:59 +0200

    Backend: Added TripAddOrUpdate passenger subController

---
+++ backend/app/Http/Controllers/PassengerControllers/TripAddOrUpdate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PassengerControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TripAddOrUpdate extends Controller
+{
+    //
+}

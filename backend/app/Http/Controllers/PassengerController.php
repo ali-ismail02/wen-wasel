e40386a68903c3c3ffa2f96c9883f655ba23490a
ali-ismail02 <alii.ismail02@gmail.com>
2022-11-30 18:04:54 +0200

    Backend: ProfileController => Added passngerSignup API

--- backend/app/Http/Controllers/PassengerController.php
+++ backend/app/Http/Controllers/PassengerController.php
@@ -19,47 +19,6 @@ use Validator;
 
 class PassengerController extends Controller
 {
-    // Api for passenger signup
-    public function passengerSignUp(Request $request){
-        // Validate the request
-        $rules = [
-            'email'=>'required|email:rfc,dns|unique:users',
-            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/', // at least 1 uppercase, 1 lowercase, 1 number
-            'phone' => 'required|min:8|max:8',
-            'name' => 'required',
-        ];
-        if($error = validate($request->all(), $rules)){
-            return response()->json([
-                "status" => "Failed",
-                "message" => "Validation Failed",
-                "errors" => $error
-            ]);
-        }
-
-        // Create new user
-        $user = User::create([ // ====================================================================
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-            'phone' => $request->phone,
-            'name' => $request->name,
-            'user_type' => 2,
-            'image' => 'default.png',
-        ]);
-        if(!$user){
-            return response()->json([
-                "status" => "Failed",
-                "message" => "Failed to create user"
-            ]);
-        }
-
-        // Create a token for the user to avoid another request for login
-        $token = JWTAuth::fromUser($user);
-
-        return response()->json([
-            'token' => $token,
-            'user' => $user,
-        ], 200);
-    }
     
     // Api to add a trip
     public function addTrip(Request $request){
